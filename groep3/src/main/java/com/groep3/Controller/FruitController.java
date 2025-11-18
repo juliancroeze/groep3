0bc210811d076fde65dc301b6b58fb71e7df9db1
Julian Croeze <croezejulian@gmail.com>
2025-11-18 14:17:06 +0100

    Add json file to FruitController

--- groep3/src/main/java/com/groep3/Controller/FruitController.java
+++ groep3/src/main/java/com/groep3/Controller/FruitController.java
@@ -1,19 +1,25 @@
 package com.groep3.Controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.groep3.App;
 import com.groep3.Model.Fruit;
 
 public class FruitController {
     private List<Fruit> fruits;
 
     public FruitController() {
-        fruits = List.of(
-            new Fruit("Apple", "A sweet red fruit", "/images/apple.png", 0.79),
-            new Fruit("Banana", "Lange gele banaan", "/images/banana.png", 0.29),
-            new Fruit("Orange", "Heerlijke sinaasappel", "/images/orange.png", 0.99),
-            new Fruit("Kiwi", "Groene kiwi met pitjes", "/images/kiwi.png", 1.49)
-        );
+        fruits = new ArrayList<Fruit>();
+        ObjectMapper objectMapper = new ObjectMapper();
+        try{
+            fruits = List.of(
+                objectMapper.readValue(App.class.getResourceAsStream("data/fruits.json"), Fruit[].class)
+            );
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public List<Fruit> getFruits() {
--- groep3/src/main/java/module-info.java
+++ groep3/src/main/java/module-info.java
@@ -1,7 +1,19 @@
 module com.groep3 {
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.core;
+    //    requires com.fasterxml.jackson.annotations;
     requires javafx.controls;
     requires javafx.fxml;
-    opens com.groep3.Controller to javafx.fxml;
-    opens com.groep3 to javafx.fxml;
+
+    // export the root package so javafx.graphics can access App
     exports com.groep3;
+
+    // export model if you need it publicly
+    exports com.groep3.Model;
+
+    // controllers are instantiated/reflected by FXMLLoader: open to javafx.fxml
+    opens com.groep3.Controller to javafx.fxml;
+
+    // open root package for reflection by javafx.graphics / javafx.fxml
+    opens com.groep3 to javafx.graphics, javafx.fxml;
 }
