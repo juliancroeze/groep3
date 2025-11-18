83f1b599d873d0250614eb865bf117b61113f64e
Spartac <spartac@mylermedia.nl>
2025-11-18 15:32:22 +0100

    remove old list

--- groep3/src/main/java/com/groep3/Controller/FruitController.java
+++ groep3/src/main/java/com/groep3/Controller/FruitController.java
@@ -11,20 +11,7 @@ public class FruitController {
     private List<Fruit> fruits;
 
     public FruitController() {
-        fruits = List.of(
-            new Fruit("Apple", "A sweet red fruit", "/images/apple.png", 0.79),
-            new Fruit("Banana", "Lange gele banaan", "/images/banana.png", 0.29),
-            new Fruit("Orange", "Heerlijke sinaasappel", "/images/orange.png", 0.99),
-            new Fruit("Kiwi", "Groene kiwi met pitjes", "/images/kiwi.png", 1.49),
-            new Fruit("Apple", "A sweet red fruit", "/images/apple.png", 0.79),
-            new Fruit("Banana", "Lange gele banaan", "/images/banana.png", 0.29),
-            new Fruit("Orange", "Heerlijke sinaasappel", "/images/orange.png", 0.99),
-            new Fruit("Kiwi", "Groene kiwi met pitjes", "/images/kiwi.png", 1.49),
-            new Fruit("Apple", "A sweet red fruit", "/images/apple.png", 0.79),
-            new Fruit("Banana", "Lange gele banaan", "/images/banana.png", 0.29),
-            new Fruit("Orange", "Heerlijke sinaasappel", "/images/orange.png", 0.99),
-            new Fruit("Kiwi", "Groene kiwi met pitjes", "/images/kiwi.png", 1.49)
-        );
+
         fruits = new ArrayList<Fruit>();
         ObjectMapper objectMapper = new ObjectMapper();
         try{
