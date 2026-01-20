89c5850cb531d6e9aa2413d6a6da935b585f1108
Mischa Lasschuit <104681654+MischaLas@users.noreply.github.com>
2026-01-20 19:09:10 +0100

    Kleine cleancode wijziging

--- groep3/src/main/java/com/groep3/views/FilterView.java
+++ groep3/src/main/java/com/groep3/views/FilterView.java
@@ -29,13 +29,6 @@ public class FilterView {
         ImageView iconZAView = new ImageView(iconZA);
         ImageView iconPriceHighLowView = new ImageView(iconPriceHighLow);
         ImageView iconPriceLowHighView = new ImageView(iconPriceLowHigh);
-        //filterButton.setGraphic(imageView);
-
-        //make icons for menu items
-        //menuItemAZ.setGraphic(iconAZView);
-        //menuItemZA.setGraphic(iconZAView);
-        //menuItemPriceHL.setGraphic(iconPriceHighLowView);
-        //menuItemPriceLH.setGraphic(iconPriceLowHighView);
 
     }
 
